6bcb53affa05b822ae2cfee1ce7cd80f7927a5af
maybetree <maybetree48@proton.me>
2025-08-21 12:10:13 +0200

    Update nitrokey

--- nitropy/install-udev-rule.sh
+++ nitropy/install-udev-rule.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if command -v doas > /dev/null
+then
+	alias sudo=doas
+elif command -v sudo > /dev/null
+then
+	alias doas=sudo
+else
+	echo "Neither sudo no doas found" > /dev/stderr
+fi
+
 wget https://raw.githubusercontent.com/Nitrokey/nitrokey-udev-rules/main/41-nitrokey.rules
 sudo mkdir -p /etc/udev/rules.d
 sudo mv ./41-nitrokey.rules /etc/udev/rules.d/
--- nitropy/run.sh
+++ nitropy/run.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
 # No `-ti` to keep stdout and stderr separate
+
+
+find_nitrokey() {
+	for dev in /dev/hidraw*
+	do
+		udevadm info --query=all $dev | grep ID_SECURITY_TOKEN=1 > /dev/null 2>&1
+		if [ "$?" = 0 ]
+		then
+			echo $dev
+			return
+		fi
+	done
+	echo "No nitrokey!!" > /dev/stderr
+	exit 1
+}
+
+key=$(find_nitrokey)
+
+if [ -z "$key" ]
+then
+	echo "Nitrokey not found!!" > /dev/stderr
+	exit 1
+fi
+
 podman run \
 	--rm \
-	--device /dev/hidraw0 `#FIXME might not be 0` \
+	--device "$key" \
 	maybetree-nitropy "$@"
 
 
