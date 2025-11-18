ed0fefe2d60b455dc7b5883ce9f766857be2fd97
maybetree <maybetree48@proton.me>
2025-11-18 08:46:15 +0100

    added ocrmypdf to pdftools

---
+++ pdftools/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sf $(realpath ./run.sh) ~/bin/pdftk
+ln -sf $(realpath ./run.sh) ~/bin/img2pdf
+ln -sf $(realpath ./run.sh) ~/bin/pandoc
+ln -sf $(realpath ./run.sh) ~/bin/ocrmypdf
+
